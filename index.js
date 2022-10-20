383e50c58e055340cb4d5ebf3631ae39762a9f75
rebeccaacero1 <rebeccaacero1@gmail.com>
2022-10-20 19:20:41 +0800

    add res.send hello h1

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get("/",function(request,responce){
-    console.log(request);
-    responce.send('Hello');
+    /* console.log(request);
+    responce.send('Hello'); */
+    res.send('<h1>hello?</h1>');
 })
 
 app.listen(3000, function(){
