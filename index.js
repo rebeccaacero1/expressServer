ab2a6685772a68fda0b6c353e524e77b074107f9
rebeccaacero1 <rebeccaacero1@gmail.com>
2022-10-20 20:03:15 +0800

    add about page in port 3000

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.get("/",function(request,responce){
+app.get("/",function(req,res){
     /* console.log(request);
     responce.send('Hello'); */
-    res.send('<h1>hello?</h1>');
-})
+    res.send('<h1>hello??</h1>');
+});
+
+app.get("/contact",function(req,res){
+    res.send('hi, please contact me: rebeccaacero1@gmail.com');
+});
+
+app.get('/about', function(req,res){
+    res.send('i am rebecca team player also')
+});
 
 app.listen(3000, function(){
     console.log('Server started on PORT 3000');
