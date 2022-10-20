1c273435a77b2dbddaac575d49b4e9e3f4f23a30
rebeccaacero1 <rebeccaacero1@gmail.com>
2022-10-20 18:37:18 +0800

    add basic app to display hello

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.get("/",function(request,responce){
+    console.log(request);
+    responce.send('Hello');
+})
+
 app.listen(3000, function(){
     console.log('Server started on PORT 3000');
 });
