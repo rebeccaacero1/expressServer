776da535ebb8869577adcd81baad327b63109eda
rebeccaacero1 <rebeccaacero1@gmail.com>
2022-10-20 20:32:38 +0800

    add hobby page use () CS console

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ app.get('/about', function(req,res){
     res.send('i am rebecca team player also')
 });
 
+app.get('/hobbies',function(req,res){
+    res.send(`
+    <h1>Things I like doing</h1>
+    <ol>
+    <li>coding</li>
+    <li>data science</li>
+    <li>statistics</li>
+    <li>linear algebra</li>
+    <li>cooking</li>
+    `)   
+});
+
 app.listen(3000, function(){
     console.log('Server started on PORT 3000');
 });
